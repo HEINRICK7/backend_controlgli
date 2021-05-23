5fda01ac5f4f5bb39951cb76487cd6f9b1a51139
Henrique <Carlos Henrique>
2021-05-23 11:55:19 -0300

    update sessionController

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,33 +1,36 @@
-const connection = require('../database');
+const User = require('../models/User');
+
 const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json')
+
+
 
 module.exports = {
     async store(req, res) {
         const {email, password } = req.body;
 
-        const user = await connection('users')
-            .where({email:email})
-            .first()
-    
+        const user = await User.findOne({where: { email } })
+
         if(!user){
             res.status(400).json({
                 error: "Nenhum usuário com esse email"
             })
         }
-        else{
-            return bcrypt
-            .compare(password, user.password)
-            .then(isAuthenticated => {
-                if(!isAuthenticated){
-                   res.status(400).json({
-                      error: "Senha invalida!"
-                   })
-                }
-                return res.json(user)
-                   
-            })
-            
+        if(!bcrypt.compare(password, user.password)){
+            return res.status(400).send({ error: ' Invalid password' });   
+    
         }
+
+        user.password = undefined;    
+        const token = jwt.sign({ id: user.id }, authConfig.secret, {
+            expiresIn: 86400,
+           }); 
+           
+           res.send({user, token});
+            
         
     }
         
