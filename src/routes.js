7a1703a10f4e8eca4561de6c1b9446881eadeea7
HEINRICK7 <heinrickcostta@gmail.com>
2021-12-14 07:34:56 -0300

    modifield routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const routes = express.Router();
 
+const authMiddleware = require('./middlewares/auth')
+
 const UserController = require('./controllers/UserController');
 const ResultController = require('./controllers/ResultController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 routes.post('/users', UserController.store);
-routes.get('/users', UserController.index);
+routes.get('/users', authMiddleware, UserController.index);
 
-routes.post('/users/:user_id/results', ResultController.store);
-routes.get('/users/:user_id/results', ResultController.index);
-routes.delete('/results/:id', ResultController.destroy);
+routes.post('/users/:user_id/results', authMiddleware, ResultController.store);
+routes.get('/users/:user_id/results', authMiddleware, ResultController.index);
+routes.delete('/results/:id',authMiddleware, ResultController.destroy);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile',authMiddleware, ProfileController.index);
 
 routes.post('/session', SessionController.store);
 
