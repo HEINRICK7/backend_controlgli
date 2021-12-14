e00e438c68907e281370d0130128e7b5abce5619
HEINRICK7 <heinrickcostta@gmail.com>
2021-12-14 07:35:45 -0300

    modifield

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,35 +1,32 @@
-const connection = require('../database');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
+
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User')
+
+const authConfig = require('../config/auth.json')
+
 
 module.exports = {
-    async store(req, res) {
-        const {email, password } = req.body;
 
-        const user = await connection('users')
-            .where({email:email})
-            .first()
-    
-        if(!user){
-            res.status(400).json({
-                error: "Nenhum usuário com esse email"
-            })
-        }
-        else{
-            return bcrypt
-            .compare(password, user.password)
-            .then(isAuthenticated => {
-                if(!isAuthenticated){
-                   res.status(400).json({
-                      error: "Senha invalida!"
-                   })
-                }
-                return res.json(user)
-                   
-            })
-            
-        }
-        
+    async store(req, res){
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select('password');
+
+        if( !user )
+            return res.status(400).send( {error: 'User not found'});
+
+        if( !await bcrypt.compare(password, user.password))
+            return res.status(400).send({ error: ' Invalid password' });   
+
+        user.password = undefined;    
+
+        const token = jwt.sign({ id: user.id }, authConfig.secret, {
+            expiresIn: 86400,
+        })
+
+        res.send({ user, token });    
+
     }
-        
-    
 }
